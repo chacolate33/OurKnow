0fb75a0f465a26124b47735af022a8788b78299d
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-12-09 14:39:44 +0000

    fix comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  def create
+    @knowledge = Knowledge.find(params[:knowledge_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.knowledge_id = @knowledge.id
+    if @comment.save
+      @comments = Comment.where(knowledge_id: @knowledge.id).page(params[:page]).per(20)
+    else
+      @knowledge = Knowledge.find_by(id: @knowledge.id)
+      @comments = Comment.where(knowledge_id: @knowledge.id).page(params[:page]).per(20)
+    end
+  end
+
+  def destroy
+    @knowledge = Knowledge.find(params[:knowledge_id])
+    @comments = Comment.where(knowledge_id: @knowledge.id).page(params[:page]).per(20)
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.permit(:content)
+  end
 end
